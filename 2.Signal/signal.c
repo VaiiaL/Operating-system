68d4f112683ac06442ef1cd9b60095e05a3a76b2
VaiiaL <88166585+VaiiaL@users.noreply.github.com>
2023-12-27 16:45:07 +0700

    Update signal.c

--- 2.Signal/signal.c
+++ 2.Signal/signal.c
@@ -35,7 +35,7 @@ int main()
     // 0 for 1 protocol
 	int socket_fd = socket(AF_INET, SOCK_STREAM, 0);;
 	if (socket_fd < 0)
-    {
+	{
 		perror("Couldn't create socket");
 		exit(1);
 	}
@@ -52,7 +52,7 @@ int main()
 
     // bind socket and addr
 	if (bind(socket_fd, (struct sockaddr *) &server_addr, sizeof(struct sockaddr_in)) != 0)
-    {
+	{
 		perror("Couldn't bind socket");
 		exit(1);
 	}
@@ -60,9 +60,9 @@ int main()
 	listen(socket_fd, MAX_CLIENTS);
     
 	printf("Server ONLINE\n");
-    printf("Use \"nc localhost %d\" to connect to server\n", PORT);
-    printf("To stop Server use \"kill -HUP %d\"\n", getpid());
-    printf("To stop Client send \"quit\"\n\n");
+	printf("Use \"nc localhost %d\" to connect to server\n", PORT);
+	printf("To stop Server use \"kill -HUP %d\"\n", getpid());
+	printf("To stop Client send \"quit\"\n\n");
 
     ////////////////////////////////////
     // registration of a signal handler
@@ -87,7 +87,7 @@ int main()
 	char message[BUFFER_SIZE] = {0};
 
 	while(1)
-    {
+	{
 		//////////////////////////////////////////
 		// put descriptors in set
 		int maxFd = socket_fd;
@@ -96,10 +96,10 @@ int main()
 		FD_ZERO(&fds);
 		FD_SET(socket_fd, &fds);
 		for (int i = 0; i < active_clients; i++)
-        {
+		{
 			FD_SET(clients[i], &fds);
 			if (clients[i] > maxFd)
-            {
+			{
 				maxFd = clients[i];
 			}
 		}
@@ -108,36 +108,36 @@ int main()
 		//////////////////////////////////////////////////////////////////////////////////////
 		// pselect: unblock signal by using mask and wait activity from descriptors in set fds
 		// in fact pselect create a queue for different events
-        if (pselect(maxFd + 1, &fds, NULL, NULL, NULL, &mask) < 0 && errno != EINTR)
-        {
-            perror("pselect error");
-            exit(1);
-        }
+	        if (pselect(maxFd + 1, &fds, NULL, NULL, NULL, &mask) < 0 && errno != EINTR)
+	        {
+	            perror("pselect error");
+	            exit(1);
+	        }
 		//////////////////////////////////////////////////////////////////////////////////////
 
 		///////////////////////////////////
 		// work with signal
-        if (was_sighup)
-        {
-            printf("Signal received.\n");
-            was_sighup = 0;
-            break;							// leave while(1), end app
-        }
+	        if (was_sighup)
+	        {
+	            printf("Signal received.\n");
+	            was_sighup = 0;
+	            break;						// leave while(1), end app
+	        }
 		///////////////////////////////////
 
 		if (FD_ISSET(socket_fd, &fds))
-        {
+		{
 			int conn = accept(socket_fd, NULL, NULL);
-            // check for connection ok
+            		// check for connection ok
 			if (conn < 0)
-            {
+			{
 				perror("Couldn't accept new connection");
 				continue;
 			}
 
-            // check for clients limit
+            		// check for clients limit
 			if (active_clients + 1 > MAX_CLIENTS)
-            {
+			{
 				printf("New client: Too many clients -> Refused\n");
 				close(conn);
 				continue;
@@ -145,33 +145,33 @@ int main()
 
 			clients[active_clients] = conn;
 			active_clients++;
-            printf("New client: Connected -> ID#%d\n", abs(MAX_CLIENTS - conn));
+            		printf("New client: Connected -> ID#%d\n", abs(MAX_CLIENTS - conn));
 		}
 
 		/////////////////////////////////////////////////////////////////////////////////////////////
-        // messaging
+        	// messaging
 		for (int i = 0; i < active_clients; i++)
-        {
-			if (FD_ISSET(clients[i], &fds))								// if client i connected
-            {
+		{
+			if (FD_ISSET(clients[i], &fds))				// if client i connected
+			{
 				memset(&message, 0, BUFFER_SIZE);
 				ssize_t data = read(clients[i], &message, BUFFER_SIZE - 1);
 
 				////////////////////////////
-                // if got data
+                		// if got data
 				if (data > 0)
-                {
+				{
 					data--;
 					message[data] = '\0';
 				}
 				////////////////////////////
 
 				/////////////////////////////////////////////////////////////////////////
-                // if error or client send quit
+               			// if error or client send quit
 				if (data <= 0 || !strcmp("quit", message))
-                {
+				{
 					close(clients[i]);
-                    printf("Connection closed: ID#%d\n", abs(MAX_CLIENTS - clients[i]));
+					printf("Connection closed: ID#%d\n", abs(MAX_CLIENTS - clients[i]));
 
 					clients[i] = clients[active_clients - 1];
 					clients[active_clients - 1] = 0;
